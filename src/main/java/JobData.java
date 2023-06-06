5beb60227ac7ca365ff420c5a2246ba91358553c
AJT <lunegeoid@gmail.com>
2023-06-05 20:33:07 -0500

    Updated with Solutions, excluding Bonus Missions. Passing all tests locally.

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -77,9 +77,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -99,7 +99,18 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> jobEntry : allJobs) {
+
+            String aValue = jobEntry.toString().toLowerCase();
+
+            if (aValue.contains(value.toLowerCase())) {
+                jobs.add(jobEntry);
+            }
+        }
+
+        return jobs;
     }
 
     /**
